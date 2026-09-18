e1a9e119412b58c7453c03e7e9eb6665df9866d2
Danny Nguyen <nguyen.danny142@gmail.com>
2026-09-18 04:15:35 +0000

    Expand Umamusume roster with correct GameTora racewear thumbs
    Replace mismatched icons with properly mapped Original racewear
    art for all playable Umamusume (wiki horse girls only).

--- property-training/src/lib/characters.ts
+++ property-training/src/lib/characters.ts
@@ -4,129 +4,677 @@ export type UmaCharacter = {
   image: string;
 };
 
+/** Playable Umamusume only (GameTora Original racewear thumbs). */
 export const UMA_CHARACTERS: UmaCharacter[] = [
   {
-    "id": "special-week",
-    "name": "Special Week",
-    "image": "/characters/special-week.png"
+    id: "admire-groove",
+    name: "Admire Groove",
+    image: "/characters/admire-groove.png",
   },
   {
-    "id": "silence-suzuka",
-    "name": "Silence Suzuka",
-    "image": "/characters/silence-suzuka.png"
+    id: "admire-vega",
+    name: "Admire Vega",
+    image: "/characters/admire-vega.png",
   },
   {
-    "id": "tokai-teio",
-    "name": "Tokai Teio",
-    "image": "/characters/tokai-teio.png"
+    id: "agnes-digital",
+    name: "Agnes Digital",
+    image: "/characters/agnes-digital.png",
   },
   {
-    "id": "maruzensky",
-    "name": "Maruzensky",
-    "image": "/characters/maruzensky.png"
+    id: "agnes-tachyon",
+    name: "Agnes Tachyon",
+    image: "/characters/agnes-tachyon.png",
   },
   {
-    "id": "fuji-kiseki",
-    "name": "Fuji Kiseki",
-    "image": "/characters/fuji-kiseki.png"
+    id: "air-groove",
+    name: "Air Groove",
+    image: "/characters/air-groove.png",
   },
   {
-    "id": "oguri-cap",
-    "name": "Oguri Cap",
-    "image": "/characters/oguri-cap.png"
+    id: "air-messiah",
+    name: "Air Messiah",
+    image: "/characters/air-messiah.png",
   },
   {
-    "id": "gold-ship",
-    "name": "Gold Ship",
-    "image": "/characters/gold-ship.png"
+    id: "air-shakur",
+    name: "Air Shakur",
+    image: "/characters/air-shakur.png",
   },
   {
-    "id": "vodka",
-    "name": "Vodka",
-    "image": "/characters/vodka.png"
+    id: "almond-eye",
+    name: "Almond Eye",
+    image: "/characters/almond-eye.png",
   },
   {
-    "id": "daiwa-scarlet",
-    "name": "Daiwa Scarlet",
-    "image": "/characters/daiwa-scarlet.png"
+    id: "aston-machan",
+    name: "Aston Machan",
+    image: "/characters/aston-machan.png",
   },
   {
-    "id": "taiki-shuttle",
-    "name": "Taiki Shuttle",
-    "image": "/characters/taiki-shuttle.png"
+    id: "bamboo-memory",
+    name: "Bamboo Memory",
+    image: "/characters/bamboo-memory.png",
   },
   {
-    "id": "grass-wonder",
-    "name": "Grass Wonder",
-    "image": "/characters/grass-wonder.png"
+    id: "believe",
+    name: "Believe",
+    image: "/characters/believe.png",
   },
   {
-    "id": "mejiro-mcqueen",
-    "name": "Mejiro McQueen",
-    "image": "/characters/mejiro-mcqueen.png"
+    id: "biko-pegasus",
+    name: "Biko Pegasus",
+    image: "/characters/biko-pegasus.png",
   },
   {
-    "id": "el-condor-pasa",
-    "name": "El Condor Pasa",
-    "image": "/characters/el-condor-pasa.png"
+    id: "biwa-hayahide",
+    name: "Biwa Hayahide",
+    image: "/characters/biwa-hayahide.png",
   },
   {
-    "id": "tm-opera-o",
-    "name": "T.M. Opera O",
-    "image": "/characters/tm-opera-o.png"
+    id: "bubble-gum-fellow",
+    name: "Bubble Gum Fellow",
+    image: "/characters/bubble-gum-fellow.png",
   },
   {
-    "id": "symboli-rudolf",
-    "name": "Symboli Rudolf",
-    "image": "/characters/symboli-rudolf.png"
+    id: "buena-vista",
+    name: "Buena Vista",
+    image: "/characters/buena-vista.png",
   },
   {
-    "id": "seiun-sky",
-    "name": "Seiun Sky",
-    "image": "/characters/seiun-sky.png"
+    id: "calstone-light-o",
+    name: "Calstone Light O",
+    image: "/characters/calstone-light-o.png",
   },
   {
-    "id": "mayano-top-gun",
-    "name": "Mayano Top Gun",
-    "image": "/characters/mayano-top-gun.png"
+    id: "cesario",
+    name: "Cesario",
+    image: "/characters/cesario.png",
   },
   {
-    "id": "agnes-tachyon",
-    "name": "Agnes Tachyon",
-    "image": "/characters/agnes-tachyon.png"
+    id: "cheval-grand",
+    name: "Cheval Grand",
+    image: "/characters/cheval-grand.png",
   },
   {
-    "id": "mihono-bourbon",
-    "name": "Mihono Bourbon",
-    "image": "/characters/mihono-bourbon.png"
+    id: "chrono-genesis",
+    name: "Chrono Genesis",
+    image: "/characters/chrono-genesis.png",
   },
   {
-    "id": "rice-shower",
-    "name": "Rice Shower",
-    "image": "/characters/rice-shower.png"
+    id: "copano-rickey",
+    name: "Copano Rickey",
+    image: "/characters/copano-rickey.png",
   },
   {
-    "id": "winning-ticket",
-    "name": "Winning Ticket",
-    "image": "/characters/winning-ticket.png"
+    id: "curren-bouquetdor",
+    name: "Curren Bouquetd'or",
+    image: "/characters/curren-bouquetdor.png",
   },
   {
-    "id": "nice-nature",
-    "name": "Nice Nature",
-    "image": "/characters/nice-nature.png"
+    id: "curren-chan",
+    name: "Curren Chan",
+    image: "/characters/curren-chan.png",
   },
   {
-    "id": "king-halo",
-    "name": "King Halo",
-    "image": "/characters/king-halo.png"
+    id: "daiichi-ruby",
+    name: "Daiichi Ruby",
+    image: "/characters/daiichi-ruby.png",
   },
   {
-    "id": "matikanefukukitaru",
-    "name": "Matikanefukukitaru",
-    "image": "/characters/matikanefukukitaru.png"
-  }
+    id: "daitaku-helios",
+    name: "Daitaku Helios",
+    image: "/characters/daitaku-helios.png",
+  },
+  {
+    id: "daiwa-scarlet",
+    name: "Daiwa Scarlet",
+    image: "/characters/daiwa-scarlet.png",
+  },
+  {
+    id: "dantsu-flame",
+    name: "Dantsu Flame",
+    image: "/characters/dantsu-flame.png",
+  },
+  {
+    id: "daring-heart",
+    name: "Daring Heart",
+    image: "/characters/daring-heart.png",
+  },
+  {
+    id: "dream-journey",
+    name: "Dream Journey",
+    image: "/characters/dream-journey.png",
+  },
+  {
+    id: "duramente",
+    name: "Duramente",
+    image: "/characters/duramente.png",
+  },
+  {
+    id: "durandal",
+    name: "Durandal",
+    image: "/characters/durandal.png",
+  },
+  {
+    id: "eishin-flash",
+    name: "Eishin Flash",
+    image: "/characters/eishin-flash.png",
+  },
+  {
+    id: "el-condor-pasa",
+    name: "El Condor Pasa",
+    image: "/characters/el-condor-pasa.png",
+  },
+  {
+    id: "epiphaneia",
+    name: "Epiphaneia",
+    image: "/characters/epiphaneia.png",
+  },
+  {
+    id: "espoir-city",
+    name: "Espoir City",
+    image: "/characters/espoir-city.png",
+  },
+  {
+    id: "fenomeno",
+    name: "Fenomeno",
+    image: "/characters/fenomeno.png",
+  },
+  {
+    id: "fine-motion",
+    name: "Fine Motion",
+    image: "/characters/fine-motion.png",
+  },
+  {
+    id: "fuji-kiseki",
+    name: "Fuji Kiseki",
+    image: "/characters/fuji-kiseki.png",
+  },
+  {
+    id: "furioso",
+    name: "Furioso",
+    image: "/characters/furioso.png",
+  },
+  {
+    id: "fusaichi-pandora",
+    name: "Fusaichi Pandora",
+    image: "/characters/fusaichi-pandora.png",
+  },
+  {
+    id: "gentildonna",
+    name: "Gentildonna",
+    image: "/characters/gentildonna.png",
+  },
+  {
+    id: "gold-city",
+    name: "Gold City",
+    image: "/characters/gold-city.png",
+  },
+  {
+    id: "gold-ship",
+    name: "Gold Ship",
+    image: "/characters/gold-ship.png",
+  },
+  {
+    id: "gran-alegria",
+    name: "Gran Alegria",
+    image: "/characters/gran-alegria.png",
+  },
+  {
+    id: "grass-wonder",
+    name: "Grass Wonder",
+    image: "/characters/grass-wonder.png",
+  },
+  {
+    id: "haru-urara",
+    name: "Haru Urara",
+    image: "/characters/haru-urara.png",
+  },
+  {
+    id: "hishi-akebono",
+    name: "Hishi Akebono",
+    image: "/characters/hishi-akebono.png",
+  },
+  {
+    id: "hishi-amazon",
+    name: "Hishi Amazon",
+    image: "/characters/hishi-amazon.png",
+  },
+  {
+    id: "hishi-miracle",
+    name: "Hishi Miracle",
+    image: "/characters/hishi-miracle.png",
+  },
+  {
+    id: "hokko-tarumae",
+    name: "Hokko Tarumae",
+    image: "/characters/hokko-tarumae.png",
+  },
+  {
+    id: "ikuno-dictus",
+    name: "Ikuno Dictus",
+    image: "/characters/ikuno-dictus.png",
+  },
+  {
+    id: "inari-one",
+    name: "Inari One",
+    image: "/characters/inari-one.png",
+  },
+  {
+    id: "ines-fujin",
+    name: "Ines Fujin",
+    image: "/characters/ines-fujin.png",
+  },
+  {
+    id: "jungle-pocket",
+    name: "Jungle Pocket",
+    image: "/characters/jungle-pocket.png",
+  },
+  {
+    id: "ksmiracle",
+    name: "K.S.Miracle",
+    image: "/characters/ksmiracle.png",
+  },
+  {
+    id: "katsuragi-ace",
+    name: "Katsuragi Ace",
+    image: "/characters/katsuragi-ace.png",
+  },
+  {
+    id: "kawakami-princess",
+    name: "Kawakami Princess",
+    image: "/characters/kawakami-princess.png",
+  },
+  {
+    id: "king-halo",
+    name: "King Halo",
+    image: "/characters/king-halo.png",
+  },
+  {
+    id: "kiseki",
+    name: "Kiseki",
+    image: "/characters/kiseki.png",
+  },
+  {
+    id: "kitasan-black",
+    name: "Kitasan Black",
+    image: "/characters/kitasan-black.png",
+  },
+  {
+    id: "loves-only-you",
+    name: "Loves Only You",
+    image: "/characters/loves-only-you.png",
+  },
+  {
+    id: "lucky-lilac",
+    name: "Lucky Lilac",
+    image: "/characters/lucky-lilac.png",
+  },
+  {
+    id: "manhattan-cafe",
+    name: "Manhattan Cafe",
+    image: "/characters/manhattan-cafe.png",
+  },
+  {
+    id: "maruzensky",
+    name: "Maruzensky",
+    image: "/characters/maruzensky.png",
+  },
+  {
+    id: "marvelous-sunday",
+    name: "Marvelous Sunday",
+    image: "/characters/marvelous-sunday.png",
+  },
+  {
+    id: "matikanefukukitaru",
+    name: "Matikanefukukitaru",
+    image: "/characters/matikanefukukitaru.png",
+  },
+  {
+    id: "matikanetannhauser",
+    name: "Matikanetannhauser",
+    image: "/characters/matikanetannhauser.png",
+  },
+  {
+    id: "mayano-top-gun",
+    name: "Mayano Top Gun",
+    image: "/characters/mayano-top-gun.png",
+  },
+  {
+    id: "meisho-doto",
+    name: "Meisho Doto",
+    image: "/characters/meisho-doto.png",
+  },
+  {
+    id: "mejiro-ardan",
+    name: "Mejiro Ardan",
+    image: "/characters/mejiro-ardan.png",
+  },
+  {
+    id: "mejiro-bright",
+    name: "Mejiro Bright",
+    image: "/characters/mejiro-bright.png",
+  },
+  {
+    id: "mejiro-dober",
+    name: "Mejiro Dober",
+    image: "/characters/mejiro-dober.png",
+  },
+  {
+    id: "mejiro-mcqueen",
+    name: "Mejiro McQueen",
+    image: "/characters/mejiro-mcqueen.png",
+  },
+  {
+    id: "mejiro-palmer",
+    name: "Mejiro Palmer",
+    image: "/characters/mejiro-palmer.png",
+  },
+  {
+    id: "mejiro-ramonu",
+    name: "Mejiro Ramonu",
+    image: "/characters/mejiro-ramonu.png",
+  },
+  {
+    id: "mejiro-ryan",
+    name: "Mejiro Ryan",
+    image: "/characters/mejiro-ryan.png",
+  },
+  {
+    id: "mihono-bourbon",
+    name: "Mihono Bourbon",
+    image: "/characters/mihono-bourbon.png",
+  },
+  {
+    id: "mr-cb",
+    name: "Mr. C.B.",
+    image: "/characters/mr-cb.png",
+  },
+  {
+    id: "nakayama-festa",
+    name: "Nakayama Festa",
+    image: "/characters/nakayama-festa.png",
+  },
+  {
+    id: "narita-brian",
+    name: "Narita Brian",
+    image: "/characters/narita-brian.png",
+  },
+  {
+    id: "narita-taishin",
+    name: "Narita Taishin",
+    image: "/characters/narita-taishin.png",
+  },
+  {
+    id: "narita-top-road",
+    name: "Narita Top Road",
+    image: "/characters/narita-top-road.png",
+  },
+  {
+    id: "neo-universe",
+    name: "Neo Universe",
+    image: "/characters/neo-universe.png",
+  },
+  {
+    id: "nice-nature",
+    name: "Nice Nature",
+    image: "/characters/nice-nature.png",
+  },
+  {
+    id: "nishino-flower",
+    name: "Nishino Flower",
+    image: "/characters/nishino-flower.png",
+  },
+  {
+    id: "no-reason",
+    name: "No Reason",
+    image: "/characters/no-reason.png",
+  },
+  {
+    id: "north-flight",
+    name: "North Flight",
+    image: "/characters/north-flight.png",
+  },
+  {
+    id: "oguri-cap",
+    name: "Oguri Cap",
+    image: "/characters/oguri-cap.png",
+  },
+  {
+    id: "orfevre",
+    name: "Orfevre",
+    image: "/characters/orfevre.png",
+  },
+  {
+    id: "red-desire",
+    name: "Red Desire",
+    image: "/characters/red-desire.png",
+  },
+  {
+    id: "rhein-kraft",
+    name: "Rhein Kraft",
+    image: "/characters/rhein-kraft.png",
+  },
+  {
+    id: "rice-shower",
+    name: "Rice Shower",
+    image: "/characters/rice-shower.png",
+  },
+  {
+    id: "royce-and-royce",
+    name: "Royce and Royce",
+    image: "/characters/royce-and-royce.png",
+  },
+  {
+    id: "rulership",
+    name: "Rulership",
+    image: "/characters/rulership.png",
+  },
+  {
+    id: "sakura-bakushin-o",
+    name: "Sakura Bakushin O",
+    image: "/characters/sakura-bakushin-o.png",
+  },
+  {
+    id: "sakura-chiyono-o",
+    name: "Sakura Chiyono O",
+    image: "/characters/sakura-chiyono-o.png",
+  },
+  {
+    id: "sakura-laurel",
+    name: "Sakura Laurel",
+    image: "/characters/sakura-laurel.png",
+  },
+  {
+    id: "satono-crown",
+    name: "Satono Crown",
+    image: "/characters/satono-crown.png",
+  },
+  {
+    id: "satono-diamond",
+    name: "Satono Diamond",
+    image: "/characters/satono-diamond.png",
+  },
+  {
+    id: "seeking-the-pearl",
+    name: "Seeking the Pearl",
+    image: "/characters/seeking-the-pearl.png",
+  },
+  {
+    id: "seiun-sky",
+    name: "Seiun Sky",
+    image: "/characters/seiun-sky.png",
+  },
+  {
+    id: "shinko-windy",
+    name: "Shinko Windy",
+    image: "/characters/shinko-windy.png",
+  },
+  {
+    id: "silence-suzuka",
+    name: "Silence Suzuka",
+    image: "/characters/silence-suzuka.png",
+  },
+  {
+    id: "sirius-symboli",
+    name: "Sirius Symboli",
+    image: "/characters/sirius-symboli.png",
+  },
+  {
+    id: "smart-falcon",
+    name: "Smart Falcon",
+    image: "/characters/smart-falcon.png",
+  },
+  {
+    id: "sounds-of-earth",
+    name: "Sounds of Earth",
+    image: "/characters/sounds-of-earth.png",
+  },
+  {
+    id: "special-week",
+    name: "Special Week",
+    image: "/characters/special-week.png",
+  },
+  {
+    id: "stay-gold",
+    name: "Stay Gold",
+    image: "/characters/stay-gold.png",
+  },
+  {
+    id: "still-in-love",
+    name: "Still in Love",
+    image: "/characters/still-in-love.png",
+  },
+  {
+    id: "super-creek",
+    name: "Super Creek",
+    image: "/characters/super-creek.png",
+  },
+  {
+    id: "sweep-tosho",
+    name: "Sweep Tosho",
+    image: "/characters/sweep-tosho.png",
+  },
+  {
+    id: "symboli-kris-s",
+    name: "Symboli Kris S",
+    image: "/characters/symboli-kris-s.png",
+  },
+  {
+    id: "symboli-rudolf",
+    name: "Symboli Rudolf",
+    image: "/characters/symboli-rudolf.png",
+  },
+  {
+    id: "taiki-shuttle",
+    name: "Taiki Shuttle",
+    image: "/characters/taiki-shuttle.png",
+  },
+  {
+    id: "tamamo-cross",
+    name: "Tamamo Cross",
+    image: "/characters/tamamo-cross.png",
+  },
+  {
+    id: "tanino-gimlet",
+    name: "Tanino Gimlet",
+    image: "/characters/tanino-gimlet.png",
+  },
+  {
+    id: "tap-dance-city",
+    name: "Tap Dance City",
+    image: "/characters/tap-dance-city.png",
+  },
+  {
+    id: "tm-opera-o",
+    name: "TM Opera O",
+    image: "/characters/tm-opera-o.png",
+  },
+  {
+    id: "tokai-teio",
+    name: "Tokai Teio",
+    image: "/characters/tokai-teio.png",
+  },
+  {
+    id: "tosen-jordan",
+    name: "Tosen Jordan",
+    image: "/characters/tosen-jordan.png",
+  },
+  {
+    id: "transcend",
+    name: "Transcend",
+    image: "/characters/transcend.png",
+  },
+  {
+    id: "tsurumaru-tsuyoshi",
+    name: "Tsurumaru Tsuyoshi",
+    image: "/characters/tsurumaru-tsuyoshi.png",
+  },
+  {
+    id: "twin-turbo",
+    name: "Twin Turbo",
+    image: "/characters/twin-turbo.png",
+  },
+  {
+    id: "verxina",
+    name: "Verxina",
+    image: "/characters/verxina.png",
+  },
+  {
+    id: "victoire-pisa",
+    name: "Victoire Pisa",
+    image: "/characters/victoire-pisa.png",
+  },
+  {
+    id: "vivlos",
+    name: "Vivlos",
+    image: "/characters/vivlos.png",
+  },
+  {
+    id: "vodka",
+    name: "Vodka",
+    image: "/characters/vodka.png",
+  },
+  {
+    id: "win-variation",
+    name: "Win Variation",
+    image: "/characters/win-variation.png",
+  },
+  {
+    id: "winning-ticket",
+    name: "Winning Ticket",
+    image: "/characters/winning-ticket.png",
+  },
+  {
+    id: "wonder-acute",
+    name: "Wonder Acute",
+    image: "/characters/wonder-acute.png",
+  },
+  {
+    id: "yaeno-muteki",
+    name: "Yaeno Muteki",
+    image: "/characters/yaeno-muteki.png",
+  },
+  {
+    id: "yamanin-zephyr",
+    name: "Yamanin Zephyr",
+    image: "/characters/yamanin-zephyr.png",
+  },
+  {
+    id: "yukino-bijin",
+    name: "Yukino Bijin",
+    image: "/characters/yukino-bijin.png",
+  },
+  {
+    id: "zenno-rob-roy",
+    name: "Zenno Rob Roy",
+    image: "/characters/zenno-rob-roy.png",
+  },
 ];
 
-export function getCharacter(id: string | null | undefined) {
-  return UMA_CHARACTERS.find((c) => c.id === id) ?? null;
+export function getCharacter(id: string | null | undefined): UmaCharacter | undefined {
+  if (!id) return undefined;
+  return UMA_CHARACTERS.find((c) => c.id === id);
 }
+
